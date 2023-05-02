3c05d6e133c0dca97b2decb895aa938e89f28465
Cimorexave <sadeq.s502@gmail.com>
2023-05-02 15:41:40 +0430

    indexed foreach method; feat: optional index

---
+++ Utils/Loop.cs
@@ -0,0 +1,12 @@
+﻿namespace Utils;
+
+public class Loop {
+    public static void iForEach<T>(this IEnumerable<T> enumerable, Action<T, int?> action)
+    {
+        int index = 0;
+        foreach (T item in enumerable)
+        {
+            action(item, index++);
+        }
+    }
+}
