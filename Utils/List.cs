458f2de3982dfc27f31d38c9934a282d5988b799
Cimorexave <sadeq.s502@gmail.com>
2023-05-02 15:41:56 +0430

    list slice

---
+++ Utils/List.cs
@@ -0,0 +1,11 @@
+namespace Utils;
+
+public class List {
+    public static List<T> Slice<T>(this IEnumerable<T> source, int start = 0, int end = 0)
+    {
+        start = (start >= 0) ? start : source.Count() + start;
+        end = (end > 0) ? end : source.Count() + end;
+
+        return source.Skip(start).Take(end - start).ToList();
+    }
+}
