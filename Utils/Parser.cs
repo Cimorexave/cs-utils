03fbfc1d488f5e3fb602fe8131867d66da7eb363
Cimorexave <sadeq.s502@gmail.com>
2023-05-02 15:42:11 +0430

    German floatParser

---
+++ Utils/Parser.cs
@@ -0,0 +1,9 @@
+namespace Utils;
+
+public class Parser {
+    public static float parseFloat(this string source)
+    {
+        if (source.Contains(",")) return float.Parse(source.Replace(",","."));
+        else return float.Parse(source);
+    }
+}
