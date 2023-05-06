db0645551505db4e0857c3dae128e67c41d9d7a9
Cimorexave <sadeq.s502@gmail.com>
2023-05-06 16:56:23 +0430

    log and count utils

--- Utils/Log.cs
+++ Utils/Log.cs
@@ -1,10 +1,17 @@
 namespace Utils;
 
 public static class Log {
+    private static int callsCount = 0;
     public static void print (this string source, string? label) {
         if (label == null) Console.WriteLine(source);
         else {
             Console.WriteLine($"{label} => {source}");
         }
+    }
+    public static void count () {
+        Console.WriteLine(++callsCount);
+    }
+    public static void log(string msg = "Here! ----->") {
+        Console.WriteLine(msg);
     }
 }
